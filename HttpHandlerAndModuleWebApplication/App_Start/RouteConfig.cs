36df56a3a98752d1ee77826edeb3ef36c86764a1
Bartek Woźniak <bartekwozniak93@gmail.com>
2020-10-25 11:55:43 +0100

    Add project files.

---
+++ HttpHandlerAndModuleWebApplication/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HttpHandlerAndModuleWebApplication
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{assets}", new { assets = @".*\.(pdf|doc)(/.)?" });
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}"
+            );
+        }
+    }
+}
---
+++ HttpHandlerAndModuleWebApplication/Handlers/MyHttpHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+
+namespace HttpHandlerAndModuleWebApplication.Handlers
+{
+    public class MyHttpHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var response = context.Response;
+            response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
+            response.Write("<h1>HttpHandler Test</h1>");
+            response.Write($"HttpStatusCode: { response.StatusCode}<br>");
+            response.Write($"CurrentExecutionFilePathExtension: {context.Request.CurrentExecutionFilePathExtension}<br>");
+            response.Write($"Type: {context.Request.Browser.Type}<br>");
+            response.Write($"Version: {context.Request.Browser.Version}");
+        }
+    }
+}
